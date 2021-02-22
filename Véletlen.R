a72489f5155d9a68eeb8c65f6ac431ce9431bdc1
Péter Kalicz <kaliczp@gmail.com>
2021-02-22 15:19:54 +0100

    Órai munka feltöltése

--- Véletlen.R
+++ Véletlen.R
@@ -1,1 +1,10 @@
-rnorm(10)
+## Órai munka, minta a házihoz
+mo = scan(file = "mo2000.txt")
+set.seed(1)
+mo.norm = rnorm(length(mo), mean(mo), sd(mo))
+plot( mo, type = "l", xlab = "t [nap]", ylab = "h [cm]", lwd = 2)
+lines(mo.norm)
+summary(mo)
+sd(mo)*2
+IQR(mo)
+summary(mo.norm)
